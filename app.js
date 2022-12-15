d747d1a7092bbda5e53cc2dd41e6d800d66de53a
Rakeem <rakeemshaikh4545@gmail.com>
2022-12-15 22:25:40 +0530

    ejs challenge added database using mongoose

--- app.js
+++ app.js
@@ -3,13 +3,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const { lowerCase } = require("lodash");
+const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const posts = [];
 
 const app = express();
 
@@ -18,8 +18,58 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
+//creating a db named blogs
+mongoose.set('strictQuery', true);
+mongoose.connect("mongodb://localhost:27017/blogs");
+
+const blogSchema = new mongoose.Schema({
+  blogTitle: String,
+  article: String
+});
+
+const Blog = mongoose.model("Blog",blogSchema);
+
+const blog = [
+  {
+    blogTitle:"day1",
+    article:"this is the article for the first day"
+  },
+  {
+    blogTitle:"day2",
+    article:"this is the article for the second day"
+  },
+  {
+    blogTitle:"day3",
+    article:"this is the article for the third day"
+  }
+];
+
+
+
 app.get("/",function(req,res){
-  res.render("home",{homeStartingContent:homeStartingContent,posts:posts});
+
+  Blog.find({},function(err,blogs){
+ 
+    if(blogs.length == 0){
+      //inserting the element in the Blog collection-----------
+      Blog.insertMany(blog, function(err){
+        if(!err){
+          console.log("successfully saved");
+          res.redirect("/");
+        }else{
+          console.log(err);
+        }
+      });
+
+    }
+    else{
+
+      res.render("home",{homeStartingContent:homeStartingContent,posts:blogs});
+      
+    }
+
+  })
 });
 
 app.get("/about",function(req,res){
@@ -36,33 +86,45 @@ app.get("/compose",function(req,res){
 
 //learning anout route parametering 
 app.get("/post/:postName",function(req,res){
-  const requestedTitle = lowerCase(req.params.postName);
-  posts.forEach(function(post){
-    const storedTitle = lowerCase(post.title);
-    if(storedTitle == requestedTitle){
-      res.render("post",{Title:post.title , content:post.content})
+  const requestedTitle = req.params.postName;
+  Blog.findOne({blogTitle:requestedTitle},function(err,blog){
+    if(!err){
+      if(!blog){
+        res.redirect("/compose");
+      }else{
+        res.render("post",{Title:blog.blogTitle , content:blog.article});
+      }
     }
-    else{
-      console.log("not a match");
-    }
-  });
+  })
+  // posts.forEach(function(post){
+  //   const storedTitle = _.lowerCase(post.title);
+  //   if(storedTitle == requestedTitle){
+  //     res.render("post",{Title:post.title , content:post.content})
+  //   }
+  //   else{
+  //     console.log("not a match");
+  //   }
+  // });
 });
 
 //making a post request to compose page to get the input text 
 app.post("/compose",function(req,res){
-  const post = {
-    title:req.body.textTitle,
-    content:req.body.textBody
-  };
-  posts.push(post);
+  const title = req.body.textTitle;
+  const content = req.body.textBody;
+
+  const newBlog = new Blog({
+    blogTitle:title,
+    article:content
+  });
+
+  newBlog.save();
   res.redirect("/");
+  // const post = {
+  // };
+
+  // posts.push(post);
+  // res.redirect("/");
 })
-
-
-
-
-
-
 
 
 app.listen(3000, function() {
